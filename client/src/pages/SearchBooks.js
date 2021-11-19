cc0762f6985198ba2ceac222e2ae8490290436fc
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-18 21:20:18 -0500

    reworked the search when the submit button is clicked by the user

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -28,6 +28,35 @@ const searchBooks = () => {
     if (!searchInput) {
       return false
     }
+
+    try {
+      cons response = await searchGoogleBooks(searchInput);
+      if(!response.ok) {
+        throw new Error('something went wrong!');
+      }
+      const {items} = await response.json();
+
+      const bookData = items.map((book) => ({
+        bookId: book.id,
+        authors: book.volumeInfo.authors || ['No Author to Display'],
+        title: book.volumeInfo.title,
+        description: book.volumeInfo.description,
+        image: book.volumeInfo.imageLinks?.thumbnail || '',
+      }));
+
+      setSearchedBooks(bookData);
+      setSearchInput('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSaveBook = async (bookId) => {
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+
+    try {
+      
+    }
   }
 }
 
