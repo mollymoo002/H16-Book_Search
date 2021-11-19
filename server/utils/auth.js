d955a6e4f5e8a0c64b1e11ea42e79367634b2e91
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-19 17:25:07 -0500

    reworked the auth utils file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,10 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function({ username, email, _id}) {
+    const payload = { username, email, _id};
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -16,7 +20,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,15 +29,9 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+    return req;
+  }
 };
