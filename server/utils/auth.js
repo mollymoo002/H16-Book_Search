6deb9229ee0883d502312c7ea0aac79965bddfb9
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-19 21:21:59 -0500

    fixed some errors in auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,9 +10,9 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ( {req} ) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token  || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
